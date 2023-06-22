e4ce106458185b9ec8aac4197ce059d6d006288a
Kunzzz <quoctruong310803@gmail.com>
2023-06-22 15:48:27 +0700

    fix: config path page render

--- src/main/java/com/highschool/highschoolsystem/controller/HomeController.java
+++ src/main/java/com/highschool/highschoolsystem/controller/HomeController.java
@@ -4,13 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
     @GetMapping({"", "/home"})
-    public String index(Model model) {
+    public String index(@RequestParam(value = "component", defaultValue = "", required = true) String component, Model model) {
         model.addAttribute("title", "Home");
-        return "index";
+        if (component.isEmpty()) {
+            return "index";
+        }
+        return "pages/" + component;
     }
 }
